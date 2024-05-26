7c463bd1edb33917d156a0ec7b92582693aff1d1
romero9919 <hojoong9919@naver.com>
2024-05-26 21:34:43 +0900

    nest study

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from "bcryptjs";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private jwtService: JwtService
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}>{
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ username });
+
+    if(user && (await bcrypt.compare(password, user.password))){
+      // token(secret+payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('logIn failed');
+    }
   }
 
 }
--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto{
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]/, {
+    message: '아니 비밀번호를 거지같이 쓰고 있어 a-z A-Z 0-9에 맞춰서 쓰도록 해라'
+  })
   password: string;
 }
